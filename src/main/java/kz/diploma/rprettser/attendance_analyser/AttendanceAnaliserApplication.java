af862a8c8f3684354b9d0c6919917ef9efb48291
rprettser <pr.rostislav@icloud.com>
2026-05-13 22:14:02 +0500

    timezone

--- src/main/java/kz/diploma/rprettser/attendance_analyser/AttendanceAnaliserApplication.java
+++ src/main/java/kz/diploma/rprettser/attendance_analyser/AttendanceAnaliserApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @EnableScheduling
 @SpringBootApplication
 public class AttendanceAnaliserApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone(System.getenv().getOrDefault("APP_TIMEZONE", "Europe/Almaty")));
 		SpringApplication.run(AttendanceAnaliserApplication.class, args);
 	}
 
