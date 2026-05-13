75003dcf0391fc389b887fe761a9755a9ab03777
rprettser <pr.rostislav@icloud.com>
2026-05-13 22:09:23 +0500

    timezone

---
+++ src/main/java/kz/diploma/rprettser/attendance_analyser/config/TimezoneConfig.java
@@ -0,0 +1,19 @@
+package kz.diploma.rprettser.attendance_analyser.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
+
+@Configuration
+public class TimezoneConfig {
+
+    @Value("${attendance.timezone:Europe/Almaty}")
+    private String timezone;
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone(timezone));
+    }
+}
